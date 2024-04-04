35c145b4606817db80f38e2bb9b5a63c7c78a0df
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-04 10:35:39 +0700

    create function and route to short url

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
-import express from "express";
+import express, { json } from "express";
+import { nanoid } from "nanoid";
 
 const app = express();
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World");
-});
+let urlMap = new Map();
+
+const shortUrl = (req: express.Request, res: express.Response) => {
+  const { url } = req.body;
+
+  if (url) {
+    const urlId = nanoid(10);
+    urlMap.set(urlId, url);
+
+    res.contentType("application/json").status(200);
+    res.json({
+      success: true,
+      message: "successfully registered",
+      shorten: urlId,
+    });
+  } else {
+    res.contentType("application/json").status(400);
+    res.json({
+      success: false,
+      message: "url is not specified",
+    });
+  }
+};
+
+app.post("/shorten", json(), shortUrl);
 
 const PORT = 3000;
 
