3c86b1c7970360f34070b9da0bb27f82d72e565d
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-04 10:36:33 +0700

    create function and route to redirect shorted url

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,24 @@ const shortUrl = (req: express.Request, res: express.Response) => {
   }
 };
 
+const getUrl = (req: express.Request, res: express.Response) => {
+  const { short_url } = req.params;
+  const url = urlMap.get(short_url);
+
+  if (url) {
+    res.status(301);
+    res.redirect(url);
+  } else {
+    res.contentType("application/json").status(404);
+    res.json({
+      success: false,
+      message: "url is not registered",
+    });
+  }
+};
+
 app.post("/shorten", json(), shortUrl);
+app.get("/shorten/:short_url", getUrl);
 
 const PORT = 3000;
 
