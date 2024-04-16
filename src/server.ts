30928a482cf1c393ce4669889bab5b84a8263bee
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-16 09:41:17 +0700

    refactor logger middleware

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
-import express, { json, Request, Response, NextFunction } from "express";
+import express, { json } from "express";
 import { nanoid } from "nanoid";
+import { logRequest } from "./middlewares/logger";
 
 const app = express();
 
 let urlMap = new Map();
 
-const logRequest = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next();
-};
-
 const shortUrl = (req: express.Request, res: express.Response) => {
   const { url } = req.body;
-                    
+
   if (url) {
     const urlId = nanoid(10);
     urlMap.set(urlId, url);
@@ -48,7 +44,7 @@ const getUrl = (req: express.Request, res: express.Response) => {
   }
 };
 
-app.use(logRequest); 
+app.use(logRequest);
 
 app.post("/shorten", json(), shortUrl);
 app.get("/shorten/:short_url", getUrl);
