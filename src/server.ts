9b95cd922ce6baa63debd5e88d1ff96dffacfe2d
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-16 09:24:58 +0700

    create logger function

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express, { json } from "express";
+import express, { json, Request, Response, NextFunction } from "express";
 import { nanoid } from "nanoid";
 
 const app = express();
 
 let urlMap = new Map();
+
+const logRequest = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+};
 
 const shortUrl = (req: express.Request, res: express.Response) => {
   const { url } = req.body;
@@ -43,10 +48,11 @@ const getUrl = (req: express.Request, res: express.Response) => {
   }
 };
 
+app.use(logRequest); 
 app.post("/shorten", json(), shortUrl);
 app.get("/shorten/:short_url", getUrl);
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
