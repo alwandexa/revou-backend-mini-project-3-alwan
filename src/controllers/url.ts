e02bb09c9330200f1fe9f0738bf28a7eed2c2c3b
alwandexa <alwan.wirawan@djembatandua.com>
2024-04-16 09:46:42 +0700

    refactor urls function

---
+++ src/controllers/url.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { nanoid } from "nanoid";
+
+let urlMap = new Map();
+
+const shortUrl = (req: Request, res: Response) => {
+  const { url } = req.body;
+
+  if (url) {
+    const urlId = nanoid(10);
+    urlMap.set(urlId, url);
+
+    res.contentType("application/json").status(200);
+    res.json({
+      success: true,
+      message: "successfully registered",
+      shorten: urlId,
+    });
+  } else {
+    res.contentType("application/json").status(200);
+    res.json({
+      success: false,
+      message: "url is not specified",
+    });
+  }
+};
+
+const getUrl = (req: Request, res: Response) => {
+  const { short_url } = req.params;
+  const url = urlMap.get(short_url);
+
+  if (url) {
+    res.status(301);
+    res.redirect(url);
+  } else {
+    res.contentType("application/json").status(404);
+    res.json({
+      success: false,
+      message: "url is not registered",
+    });
+  }
+};
+
+export { shortUrl, getUrl };
--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,9 @@
 import express, { json } from "express";
-import { nanoid } from "nanoid";
+
 import { logRequest } from "./middlewares/logger";
+import { getUrl, shortUrl } from "./controllers/url";
 
 const app = express();
-
-let urlMap = new Map();
-
-const shortUrl = (req: express.Request, res: express.Response) => {
-  const { url } = req.body;
-
-  if (url) {
-    const urlId = nanoid(10);
-    urlMap.set(urlId, url);
-
-    res.contentType("application/json").status(200);
-    res.json({
-      success: true,
-      message: "successfully registered",
-      shorten: urlId,
-    });
-  } else {
-    res.contentType("application/json").status(200);
-    res.json({
-      success: false,
-      message: "url is not specified",
-    });
-  }
-};
-
-const getUrl = (req: express.Request, res: express.Response) => {
-  const { short_url } = req.params;
-  const url = urlMap.get(short_url);
-
-  if (url) {
-    res.status(301);
-    res.redirect(url);
-  } else {
-    res.contentType("application/json").status(404);
-    res.json({
-      success: false,
-      message: "url is not registered",
-    });
-  }
-};
 
 app.use(logRequest);
 
